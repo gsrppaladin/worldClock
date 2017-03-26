3541efc54ef91932c758a02b3352375026baaa4a
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-03-26 16:44:20 -0400

    configuring the time zone view controller

--- worldClock/TimeZonesVC.swift
+++ worldClock/TimeZonesVC.swift
@@ -7,89 +7,85 @@
 //
 
 import UIKit
+protocol WorldClockProtocol {
+    func addTimeZone(timeZone: String)
+}
 
-class TimeZonesVC: UITableViewController {
+class TimeZonesVC: UITableViewController, UISearchBarDelegate {
 
+    
+    var timeZones: [String] = []
+    
+    
+    @IBOutlet var searchTxt: UISearchBar!
+    var delegate: WorldClockProtocol?
+  
+    
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+       searchTxt.delegate = self
+        
+       timeZones = NSTimeZone.knownTimeZoneNames
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return timeZones.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
 
+        
+        cell.textLabel?.text = timeZones[indexPath.row]
+        
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+  
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.dismiss(animated: true, completion: nil)
+        //only be controlled by the module viewController.
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    //Mark: UISearchBarDelegate methods
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText != "" {
+            
+            timeZones = NSTimeZone.knownTimeZoneNames.filter { $0.contains(searchText) }
+            
+        } else {
+            timeZones = NSTimeZone.knownTimeZoneNames
+        }
+        
+        tableView.reloadData()
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- worldClock/WorldClockTVC.swift
+++ worldClock/WorldClockTVC.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+
+
+
 class WorldClockTVC: UITableViewCell {
 
     @IBOutlet var timeZoneName: UILabel!
