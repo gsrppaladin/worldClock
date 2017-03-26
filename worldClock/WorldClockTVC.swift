d559a022a725f07fcf356469e8d4884a5bd8ad56
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-03-26 15:38:55 -0400

    setting up the timer in the tableView.

--- worldClock/WorldClockTVC.swift
+++ worldClock/WorldClockTVC.swift
@@ -14,12 +14,39 @@ class WorldClockTVC: UITableViewCell {
     
     @IBOutlet var timeLabel: UILabel!
     
-    
-    
+       
     override func awakeFromNib() {
-   
         super.awakeFromNib()
         // Initialization code
+        
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(setTime), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: RunLoopMode.commonModes)
+    }
+    
+    
+    func setTime() {
+        timeLabel.text = getTime()
+        
+    }
+    
+    func getTime() -> String {
+        var timeString = ""
+        
+        if timeZoneName.text != "" {
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .long //test this line to see which one you like more. 
+            formatter.timeZone = TimeZone(identifier: timeZoneName.text!)
+            
+            let timeNow = Date()
+            timeString = formatter.string(from: timeNow)
+            
+            
+            
+            
+        }
+        
+        return timeString
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
