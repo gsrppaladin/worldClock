305dd93c043f270557957004c075fc5d8779ed23
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-04-09 20:27:06 -0400

    Created the UI for the timer view controller for the world clock app

---
+++ worldClock/timerVC.swift
@@ -0,0 +1,43 @@
+//
+//  timerVC.swift
+//  worldClock
+//
+//  Created by Sam Greenhill on 4/9/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class timerVC: UIViewController {
+
+    
+    @IBOutlet var timerPicker: UIDatePicker!
+    
+    @IBOutlet var displayLbl: UILabel!
+    
+    @IBOutlet var pauseBtn: UIButton!
+    
+    @IBOutlet var startBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func pausePressed(_ sender: UIButton) {
+        
+        
+    }
+    
+
+}
